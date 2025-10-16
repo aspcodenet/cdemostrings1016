889a21639ef799d543701303b8547bef5c14f302
stefan <stefan.holmberg@systementor.se>
2025-10-16 09:09:37 +0200

    v1

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 
 int main(){
+    // PYTHON
+    // name = input("Vad heter du?")
+    // if name == "Stefan":
+    //     print("Du är riktigt cool!")
+
+
+
     int age = 12;
     char name[11];
     int b = 9;
